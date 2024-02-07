a13fe046bc8ec0452abd14f608ecd0960c468dcc
kash12rus <159225943+kash12rus@users.noreply.github.com>
2024-02-07 14:59:22 +0300

    Add files via upload
    converting from hexadecimal to decimal

---
+++ hex to dec.py
@@ -0,0 +1,5 @@
+# converting from hexadecimal to decimal
+# преобразование из шестнадцатеричного в десятичный
+hexadecimal_number = "0xfffff"  # enter a hexadecimal number - введите шестнадцатеричное число
+decimal_number = int(hexadecimal_number, 16)
+print(decimal_number)  # Output: 
