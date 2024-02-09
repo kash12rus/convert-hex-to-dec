d4cc079ed1e0c3ad59c1a8e41b06d0a44cbfa1e5
Koresh <159225943+kash12rus@users.noreply.github.com>
2024-02-09 09:21:34 +0300

    Add files via upload

---
+++ dec to hex.py
@@ -0,0 +1,11 @@
+# converting from decimal to hexadecimal
+print(hex(1234567890)[2:]) # enter a decimal number
+print(hex(234567890)[2:])
+print(hex(34567890)[2:])
+print(hex(4567890)[2:])
+print(hex(567890)[2:])
+print(hex(67890)[2:])
+print(hex(7890)[2:])
+print(hex(890)[2:])
+print(hex(90)[2:])
+print(hex(0)[2:])
